0cfd15453a3b160120db6e9baa1a2c1e5e8782f9
Luana <luanavasconcelos376@gmail.com>
2019-08-11 15:26:02 -0500

    Acrescimo da classe setor

---
+++ CasaeVideo-master/src/model/Produto.java
@@ -0,0 +1,63 @@
+package model;
+
+public class Produto {
+	
+	private int codigo;
+	private String nome;
+	private String descricao;
+	private int quantidade;
+	private String fornecedor;
+	private int tipo;
+	private String conservacao;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(String fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public String getConservacao() {
+		return conservacao;
+	}
+	public void setConservacao(String conservacao) {
+		this.conservacao = conservacao;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
